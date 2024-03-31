d969ed30ad6e56ad7cbeb1f9e689061b149de5de
fernandodelgadosoftware <fernandodelgadosoftware@gmail.com>
2024-03-31 17:47:07 -0500

    first

--- FTPUtility/FTP.cs
+++ FTPUtility/FTP.cs
@@ -7,6 +7,9 @@ using System.Net;
 using System.IO;
 using System.Collections.Generic;
 using System.Net.Http;
+using Renci.SshNet;
+using System.IO;
+using System.Threading.Tasks;
 
 namespace FTPUtility
 {
@@ -14,41 +17,98 @@ namespace FTPUtility
     public class FtpClient
     {
         private readonly string _host;
+        private readonly int _port;
         private readonly string _username;
         private readonly string _password;
         private readonly HttpClient _httpClient;
 
-        public FtpClient(string host, string username, string password)
+        public FtpClient(string host, int port, string username, string password)
         {
             _host = host;
+            _port = port;
             _username = username;
             _password = password;
             _httpClient = new HttpClient();
-            _httpClient.BaseAddress = new Uri($"ftp://{_host}/");
+            _httpClient.BaseAddress = new Uri($"sftp://{_username}@{_host}/");
         }
 
-        public async Task UploadFileAsync(string localFilePath, string remoteFilePath)
+        //public async Task UploadFileAsync(string localFilePath, string remoteFilePath)
+        //{
+        //    using (var fileStream = File.OpenRead(localFilePath))
+        //    using (var content = new StreamContent(fileStream))
+        //    {
+        //        var ftpUri = new Uri($"sftp://{_host}/{remoteFilePath}");
+        //        var requestMessage = new HttpRequestMessage(HttpMethod.Put, ftpUri);
+        //        requestMessage.Content = content;
+
+        //        requestMessage.Headers.Add("Authorization", $"Basic {GetBase64Credentials()}");
+        //        requestMessage.Headers.Add("Content-Type", "application/octet-stream");
+
+        //        using (var response = await _httpClient.SendAsync(requestMessage))
+        //        {
+        //            response.EnsureSuccessStatusCode();
+        //        }
+        //    }
+        //}
+
+        public static void SFTPUploadFileAsync()
         {
-            using (var fileStream = File.OpenRead(localFilePath))
-            using (var content = new StreamContent(fileStream))
+            //string remoteDirectoryPath = @"\C:\Users\sftpuser"; //.DellArcturus\Pictures";
+            string remoteDirectoryPath = @"/C:/Users/sftpuser";
+
+            // Local file path
+            string localFilePath = @"C:\Users\admin\source\scriptingSchoolNotes\PowerShellSQL.txt";
+
+            try
             {
-                var ftpUri = new Uri($"ftp://{_host}/{remoteFilePath}");
-                var requestMessage = new HttpRequestMessage(HttpMethod.Put, ftpUri);
-                requestMessage.Content = content;
-
-                requestMessage.Headers.Add("Authorization", $"Basic {GetBase64Credentials()}");
-                requestMessage.Headers.Add("Content-Type", "application/octet-stream");
-
-                using (var response = await _httpClient.SendAsync(requestMessage))
+                using (var client = new SftpClient("localhost", 22, "sftpuser", "password"))
                 {
-                    response.EnsureSuccessStatusCode();
+                    client.Connect();
+                    if (client.IsConnected)
+                    {
+                        Console.WriteLine("I AM CONNECTED");
+                    }
+
+                    //PrintDirectoryTree(client, remoteDirectoryPath, "");
+
+
+
+                    client.ChangeDirectory(remoteDirectoryPath);
+                    Console.WriteLine($"Changed remote directory to: {remoteDirectoryPath}");
+
+
+
+                    using (var fileStream = File.OpenRead(localFilePath))
+                    {
+                        if (fileStream != null)
+                        {
+                            Console.WriteLine("YOU ARE NOT NULL");
+                        }
+                        client.UploadFile(fileStream, Path.GetFileName(localFilePath));
+                    }
+                    client.Disconnect();
+                }
+                Console.WriteLine("File transfer successful.");
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine($"File not found: {localFilePath}");
+                Console.WriteLine($"Error message: {ex.Message}");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"An error occurred during file upload: {ex.Message}");
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine($"Inner Exception: {ex.InnerException.Message}");
                 }
             }
         }
 
+
         public async Task DownloadFileAsync(string remoteFilePath, string localFilePath)
         {
-            var ftpUri = new Uri($"ftp://{_host}/{remoteFilePath}");
+            var ftpUri = new Uri($"sftp://{_host}/{remoteFilePath}");
             var requestMessage = new HttpRequestMessage(HttpMethod.Get, ftpUri);
             requestMessage.Headers.Add("Authorization", $"Basic {GetBase64Credentials()}");
 
@@ -65,6 +125,19 @@ namespace FTPUtility
             var credentials = $"{_username}:{_password}";
             var bytes = System.Text.Encoding.UTF8.GetBytes(credentials);
             return Convert.ToBase64String(bytes);
+        }
+
+        static void PrintDirectoryTree(SftpClient client, string directory, string prefix)
+        {
+            var files = client.ListDirectory(directory);
+            foreach (var file in files)
+            {
+                Console.WriteLine(prefix + file.Name);
+                if (file.IsDirectory && file.Name != "." && file.Name != "..")
+                {
+                    PrintDirectoryTree(client, directory + "/" + file.Name, prefix + "   ");
+                }
+            }
         }
     }
 }
--- FTPUtility/Program.cs
+++ FTPUtility/Program.cs
@@ -1,6 +1,21 @@
 ﻿using FTPUtility;
 
+
 Console.WriteLine("Hello, World!");
-var ftpClient = new FtpClient("ftp.example.com", "username", "password");
-await ftpClient.UploadFileAsync(@"C:\local\file.txt", "remote/file.txt");
-await ftpClient.DownloadFileAsync("remote/file.txt", @"C:\local\file.txt");
+string filePath = @"C:\Users\admin\source\scriptingSchoolNotes\PowerShellSQL.txt";
+
+// Convert the file path to a URI-like representation
+Uri fileUri = new Uri(filePath);
+
+// Print out the URI
+Console.WriteLine("File URL: " + fileUri.ToString());
+string currentUser = Environment.UserName;
+Console.WriteLine($"Current user: {currentUser}");
+//var ftpClient = new FtpClient("localhost", 22, "sftpuser", "password");
+
+FtpClient.SFTPUploadFileAsync();
+//@"C:\Users\admin\source\scriptingSchoolNotes\PowerShellSQL.txt", @"\C:\Users\sftpuser\PowerShellSQL.txt"
+//sftp://sftpuser@localhost/C%3A/Users/sftpuser/
+//await ftpClient.DownloadFileAsync("remote/file.txt", @"C:\local\file.txt");
+Console.ReadLine();
+
